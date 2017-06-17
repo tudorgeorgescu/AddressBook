ac1d8589fdab93273683884177402e5738a07c66
tudorgeorgescu <tudorgeorgescu@Tudors-MacBook-Air.local>
2017-06-18 01:16:42 +0200

    updated backend.js for CORS

--- backend.js
+++ backend.js
@@ -4,13 +4,19 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://13.58.136.193:27017/rest_test');
-//mongoose.connect('mongodb://localhost:27017/rest_test');
+//mongoose.connect('mongodb://13.58.136.193:27017/rest_test');
+mongoose.connect('mongodb://localhost:27017/rest_test');
 
 // Express
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes
 app.use('/api', require('./routes/api'));
